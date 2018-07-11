b1c21c2f83fc1e84db1ef88009bc0d57af392725
PrateekPisat <pisatprateek12@gmail.com>
2018-07-11 17:21:02 -0400

    C++ - Python Interface Added

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 # file: build.sh
-swig -python -Isrc game_controls.i
+swig -python -c++ -Isrc game_controls.i
 
-gcc -Isrc -fPIC $(pkg-config --cflags --libs python3) -c game_controls.c game_controls_wrap.c
-gcc -shared  -fPIC -o _game_controls.so game_controls.o game_controls_wrap.o
+g++ -Isrc -fPIC $(pkg-config --cflags --libs python3) -c game_controls.cpp game_controls_wrap.cxx
+g++ -shared  -fPIC -o _game_controls.so game_controls.o game_controls_wrap.o
 
 python3 -c "import game_controls" # test
