b55a3640ac8b3f0784d1f8c6562cc9d3d9de626b
Derek Ching <ching.de@husky.neu.edu>
2018-07-12 12:21:55 -0400

    Named pipe sample communication code from cpp -> python added.

---
+++ namedpipe.py
@@ -0,0 +1,25 @@
+import os
+import errno
+
+FIFO = '/tmp/snakegame_fifo'
+
+#try:
+#    os.mkfifo(FIFO)
+#except OSError as oe:
+#    if oe.errno != errno.EEXIST:
+#        raise
+
+data_string = ""
+
+while True:
+    print("Opening FIFO...")
+    with open(FIFO) as fifo:
+        print("FIFO opened")
+        while True:
+        	# Non-blocking read.
+            data = fifo.read(1)
+            if data != '\n' and data != '':
+            	data_string += str(data)
+            else:
+            	print('Read: "{0}"'.format(data_string))
+            	data_string = ""
